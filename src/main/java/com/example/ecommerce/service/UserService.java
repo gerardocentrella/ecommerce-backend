521e68127c27e9deaa2d708e4bea0c2d7ba04e94
Gerardo Centrella <centrella.gerardo0@gmail.com>
2026-04-09 01:16:03 +0200

    Added User classes

---
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.entity.User;
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+}
